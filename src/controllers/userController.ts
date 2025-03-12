6547d440e4093ae9bf801e93945859e3617362c9
Alex Sander <alexsander01@hotmail.com.br>
2025-03-12 00:00:04 -0300

    feat: add geolocation handling to user creation.

---
+++ src/controllers/userController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { UserModel } from "../models/user";
+import lib from "../utils/lib";
+
+export const createUser = async (req: Request, res: Response) => {
+  const { name, email, address, coordinates } = req.body;
+
+  if (!address && !coordinates) {
+    return res
+      .status(400)
+      .json({ message: "Forneça endereço ou coordenadas." });
+  }
+
+  let resolvedCoordinates = coordinates;
+  if (address && !coordinates) {
+    const { lat, lng } = await lib.getCoordinatesFromAddress(address);
+    resolvedCoordinates = [lng, lat];
+  }
+
+  let resolvedAddress = address;
+  if (coordinates && !address) {
+    resolvedAddress = await lib.getAddressFromCoordinates(coordinates);
+  }
+
+  const user = new UserModel({
+    name,
+    email,
+    address: resolvedAddress,
+    coordinates: resolvedCoordinates,
+  });
+
+  await user.save();
+  return res.status(201).json(user);
+};
