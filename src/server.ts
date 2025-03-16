0e5c827f6c101b348bea5b9b12f80c13a2b4bd78
Alex Sander <alexsander01@hotmail.com.br>
2025-03-16 13:54:51 -0300

    feat: add CORS middleware for handling cross-origin requests.

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,27 @@ import userRoutes from "./routes/userRoutes";
 import regionRoutes from "./routes/regionRoutes";
 
 const server = express();
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 server.use(express.json());
+server.use(userRoutes);
+server.use(regionRoutes);
 
 server.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-
-server.use(userRoutes);
-server.use(regionRoutes);
 
 const PORT = process.env.PORT || 3000;
 
