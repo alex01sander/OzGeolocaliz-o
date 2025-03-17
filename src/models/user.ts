ab4d4cc5bf64db0a5de57c103ea17976a2de22bc
Alex Sander <alexsander01@hotmail.com.br>
2025-03-16 23:30:04 -0300

    feat: add tag User to swagger

--- src/models/user.ts
+++ src/models/user.ts
@@ -44,6 +44,7 @@ import ObjectId = mongoose.Types.ObjectId;
  * @swagger
  * /user:
  *   post:
+ *     tags: [User]
  *     summary: Register a new user
  *     description: Registers a new user with name, email, address, and/or coordinates.
  *     requestBody:
