86d6fbf4f90464a9e5383b5d4a21261288c26628
Alex Sander <alexsander01@hotmail.com.br>
2025-03-18 13:27:25 -0300

    refactor: improve address and coordinates handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,72 @@
 import { Request, Response } from "express";
 import userService from "../services/userService";
 import { StatusCodes } from "http-status-codes";
+import { UserModel } from "../models/user";
+import lib from "../utils/lib";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const user = await userService.createUser(req.body);
+    const userData = req.body;
+
+    if (
+      !userData.address &&
+      (!userData.coordinates || userData.coordinates.length !== 2)
+    ) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Provide address or coordinates." });
+    }
+
+    if (userData.address && !userData.coordinates) {
+      try {
+        const coordinates = await lib.getCoordinatesFromAddress(
+          userData.address,
+        );
+        if (!coordinates) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Internal server error",
+            error: "Coordinates not found for the address.",
+          });
+        }
+        userData.coordinates = coordinates;
+      } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: "Internal server error",
+          error: "Failed to retrieve coordinates",
+        });
+      }
+    }
+
+    if (userData.coordinates && !userData.address) {
+      try {
+        const address = await lib.getAddressFromCoordinates(
+          userData.coordinates,
+        );
+        if (!address) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Internal server error",
+            error: "Address not found for coordinates.",
+          });
+        }
+        userData.address = address;
+      } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: "Internal server error",
+          error: "Failed to retrieve address",
+        });
+      }
+    }
+
+    const user = await userService.createUser(userData);
+
     return res.status(StatusCodes.CREATED).json(user);
   } catch (error) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Error creating user", error: error.message });
+    console.error("Error creating user:", error);
+
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
   }
 };
 
@@ -58,16 +115,27 @@ export const getUserById = async (req: Request, res: Response) => {
 
 export const updateUser = async (req: Request, res: Response) => {
   try {
+    console.log("Update Request - ID:", req.params.id);
+    console.log("Update Request - Body:", req.body);
+
     const updatedUser = await userService.updateUser(req.params.id, req.body);
+
+    console.log("Updated User Result:", updatedUser);
+
     if (!updatedUser)
       return res
         .status(StatusCodes.NOT_FOUND)
         .json({ message: "User not found" });
+
     return res.status(StatusCodes.OK).json(updatedUser);
   } catch (error) {
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Error updating user", error: error.message });
+    console.error("Full Error in updateUser:", error);
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Error updating user",
+      error: error.message,
+      errorStack: error.stack,
+    });
   }
 };
 
